a684e23511b42f214a7e47f75eba84720b63a674
Vanessa F <68393728+imo9van4@users.noreply.github.com>
2021-01-19 18:28:32 -0800

    Update spam.component.ts

--- src/app/inbox/spam/spam.component.ts
+++ src/app/inbox/spam/spam.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-spam',
@@ -8,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
   })
 
   export class SpamComponent implements OnInit {
-    constructor() {}
+    register;
+    useractivityTimer;
+
+  @HostListener('window:mousemove') logoutUser() { 
+      console.log('it works');
+      clearTimeout(this.useractivityTimer);
+      this.useractivityTimer=setTimeout(()=>this.router.navigate(['login']), 180000);
+  }
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe(params => {
+      this.register = params
+      console.log(params)
+    })
+  }
         
     ngOnInit(): void {
     }
