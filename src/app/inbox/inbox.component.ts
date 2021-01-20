d53a62268583ea2e9df3df1cd4f4cbb6f06aa17a
Vanessa F <68393728+imo9van4@users.noreply.github.com>
2021-01-19 18:26:24 -0800

    Update inbox.component.ts
    service change

--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-inbox',
@@ -9,8 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 
 export class InboxComponent implements OnInit {
   register;
+  useractivityTimer;
 
-
+ @HostListener('window:mousemove') logoutUser() { 
+    console.log('it works');
+    clearTimeout(this.useractivityTimer);
+    this.useractivityTimer=setTimeout(()=>this.router.navigate(['login']), 180000);
+  }
+  
 constructor(private route: ActivatedRoute) {
   this.route.queryParams.subscribe(params => {
     this.register = params
