002ab5e41a0346ab1d8a46ca265e491a5bbf8de9
Vanessa F <68393728+imo9van4@users.noreply.github.com>
2021-01-22 18:24:40 -0800

    Add files via upload
    buggy directives

---
+++ src/app/ng-style-email.component.ts
@@ -0,0 +1,34 @@
+/*import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style-email',
+  templateUrl: './ng-style-email.component.html',
+  styleUrls: ['./ng-style-email.component.css']
+})
+export class NgStyleEmailComponent implements OnInit {
+  message: any[]=[
+    {
+    "contact": "PetalMail",
+    "subject": "Welcome to PetalMail",
+    "status": "unread"
+    },
+    {
+    "contact": "MasterCard",
+    "subject": "You owe money",
+    "status": "unread"
+    },
+    {
+    "contact": "Mom",
+    "subject": "I love you sweetie",
+    "status": "read"
+    }
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
+
+*/
